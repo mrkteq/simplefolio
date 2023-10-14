84f450fdede6f7534d3cf228c71684008ddc62f6
mrkteq <markanthonytucker@gmail.com>
2023-10-14 20:03:10 +0200

    adds more projects and animates hero text

---
+++ src/scripts/changingText.js
@@ -0,0 +1,40 @@
+var words = ['Web Developer', 'Software Developer', 'Frontend Developer', 'JavaScript Developer', 'Fullstack Developer'];
+var currentWordIndex = 0;
+var offset = 0;
+var forwards = true;
+var skipCount = 0;
+var skipDelay = 15;
+var speed = 140;
+
+function wordFlick() {
+  setInterval(function () {
+    var currentWord = words[currentWordIndex];
+
+    if (forwards) {
+      if (offset >= currentWord.length) {
+        ++skipCount;
+        if (skipCount === skipDelay) {
+          forwards = false;
+          skipCount = 0;
+        }
+      }
+    } else {
+      if (offset === 0) {
+        forwards = true;
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+      }
+    }
+
+    var part = currentWord.substr(0, offset);
+    offset += forwards ? 1 : -1;
+
+    var wordElement = document.querySelector('.word');
+    if (wordElement) {
+      wordElement.textContent = part;
+    }
+  }, speed);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  wordFlick();
+});
